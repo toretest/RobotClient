ae4c21224b48591d551df90cbc33119b9fb251b2
toretest <dragerot@gmail.com>
2026-01-13 01:59:06 +0100

    preview for å se

--- app/src/main/java/di/optimize/robotclient/RobotControlScreen.kt
+++ app/src/main/java/di/optimize/robotclient/RobotControlScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.tooling.preview.Preview
 import kotlin.math.roundToInt
 
 @Composable
@@ -173,5 +174,13 @@ fun SteeringControl() {
                 Text("H")
             }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RobotControlScreenPreview() {
+    MaterialTheme {
+        RobotControlScreen()
     }
 }
